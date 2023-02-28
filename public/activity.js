32b70169575b3d11dabf1422f625bf2ab7cc737a
kamalkantpersistent <112163213+kamalkantpersistent@users.noreply.github.com>
2023-02-28 16:25:14 +0530

    Update activity.js

--- public/activity.js
+++ public/activity.js
@@ -10,5 +10,22 @@ connection.on('initActivity', function( data ) {
 // Save Sequence
 connection.on('clickedNext', function() {
   var configuration = JSON.parse( document.getElementById( 'configuration' ).value );
+  var feedid = JSON.parse( document.getElementById( 'feedid' ).value );
+  var username = JSON.parse( document.getElementById( 'username' ).value );
+  var password = JSON.parse( document.getElementById( 'password' ).value );
+  var jobname = JSON.parse( document.getElementById( 'jobname' ).value );
+  var mobile = JSON.parse( document.getElementById( 'mobile' ).value );
+  var messages = JSON.parse( document.getElementById( 'messages' ).value );
+  var templateid = JSON.parse( document.getElementById( 'templateid' ).value );
+  var entityid = JSON.parse( document.getElementById( 'entityid' ).value );
+
   connection.trigger('updateActivity', configuration);
+  connection.trigger('feedid', feedid);
+  connection.trigger('username', username);
+  connection.trigger('password', password);
+  connection.trigger('jobname', jobname);
+  connection.trigger('mobile', mobile);
+  connection.trigger('messages', messages);
+  connection.trigger('templateid', templateid);
+  connection.trigger('entityid', entityid);
 });
